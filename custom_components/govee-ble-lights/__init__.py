a7d1410a4c5ca7b6b17722567a65e9350e906c35
Tim-Niklas Koch <info@tnkoch.de>
2022-11-16 16:01:01 +0100

    improved setup

--- custom_components/govee-ble-lights/__init__.py
+++ custom_components/govee-ble-lights/__init__.py
@@ -1,4 +1,30 @@
-"""Example Lights integration."""
+"""The Govee Bluetooth BLE integration."""
+from __future__ import annotations
 
-def setup():
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Govee BLE device from a config entry."""
+    address = entry.unique_id
+    assert address is not None
+    #do something with address
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
