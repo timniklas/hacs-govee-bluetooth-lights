de7977054cc04e9cd0aabe2144bf912c4a94c1d5
Tim-Niklas Koch <info@tnkoch.de>
2022-11-16 16:41:17 +0100

    test

--- custom_components/govee-ble-lights/__init__.py
+++ custom_components/govee-ble-lights/__init__.py
@@ -8,16 +8,17 @@ from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
+PLATFORMS: list[Platform] = [Platform.LIGHT]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Govee BLE device from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(entry, "light")
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, "light"):
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
