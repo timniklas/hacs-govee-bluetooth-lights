10505576a37fcca1256eb92848584b7cc96050af
Tim-Niklas Koch <info@tnkoch.de>
2022-11-16 15:08:27 +0100

    test

--- custom_components/govee-ble-lights/light.py
+++ custom_components/govee-ble-lights/light.py
@@ -3,60 +3,32 @@ from __future__ import annotations
 
 import logging
 
-import awesomelights
 import voluptuous as vol
 
 # Import the device class from the component that you want to support
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.light import (ATTR_BRIGHTNESS, PLATFORM_SCHEMA,
-                                            LightEntity)
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.components.light import (LightEntity)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 _LOGGER = logging.getLogger(__name__)
 
-# Validation of the user's configuration
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string,
-    vol.Optional(CONF_USERNAME, default='admin'): cv.string,
-    vol.Optional(CONF_PASSWORD): cv.string,
-})
-
 
 def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
+    add_entities: AddEntitiesCallback
 ) -> None:
     """Set up the Awesome Light platform."""
-    # Assign configuration variables.
-    # The configuration check takes care they are present.
-    host = config[CONF_HOST]
-    username = config[CONF_USERNAME]
-    password = config.get(CONF_PASSWORD)
-
-    # Setup connection with devices/cloud
-    hub = awesomelights.Hub(host, username, password)
-
-    # Verify that passed in configuration works
-    if not hub.is_valid_login():
-        _LOGGER.error("Could not connect to AwesomeLight hub")
-        return
-
     # Add devices
-    add_entities(AwesomeLight(light) for light in hub.lights())
+    add_entities(GoveeBluetoothLight())
 
 
-class AwesomeLight(LightEntity):
+class GoveeBluetoothLight():
     """Representation of an Awesome Light."""
 
     def __init__(self, light) -> None:
-        """Initialize an AwesomeLight."""
-        self._light = light
-        self._name = light.name
+        """Initialize an bluetooth light."""
+        self._name = "Light Name"
         self._state = None
         self._brightness = None
 
@@ -85,18 +57,12 @@ class AwesomeLight(LightEntity):
         You can skip the brightness part if your light does not support
         brightness control.
         """
-        self._light.brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
-        self._light.turn_on()
 
     def turn_off(self, **kwargs: Any) -> None:
         """Instruct the light to turn off."""
-        self._light.turn_off()
 
     def update(self) -> None:
         """Fetch new state data for this light.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._light.update()
-        self._state = self._light.is_on()
-        self._brightness = self._light.brightness
