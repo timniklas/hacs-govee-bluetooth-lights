68dc55d544b51141e1d0f61b5fc08df1aec0c444
Tim-Niklas Koch <info@tnkoch.de>
2022-11-16 16:03:30 +0100

    removed unload

--- custom_components/govee-ble-lights/__init__.py
+++ custom_components/govee-ble-lights/__init__.py
@@ -20,11 +20,3 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     assert address is not None
     #do something with address
     return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
